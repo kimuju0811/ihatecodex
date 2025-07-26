11c9e4a63518bae6d64cd07f49576e0b55afae41
kimuju0811 <sandmig8011@gmail.com>
2025-07-26 23:49:04 +0900

    fix keyboard dismissal overlay

--- WebtoonManager/Views/KeyboardDismissModifier.swift
+++ WebtoonManager/Views/KeyboardDismissModifier.swift
@@ -4,7 +4,7 @@ struct KeyboardDismissModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
             .background(Color.white.opacity(0.001).ignoresSafeArea())
-            .background(DismissGestureView())
+            .overlay(DismissGestureView())
     }
 }
 
