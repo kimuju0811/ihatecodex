4af403a683a31157a931cae7d912d769835c5222
kimuju0811 <sandmig8011@gmail.com>
2025-07-27 11:29:59 +0900

    Add pending rating and bump version

--- WebtoonManager/Views/HomeView.swift
+++ WebtoonManager/Views/HomeView.swift
@@ -12,7 +12,7 @@ struct HomeView: View {
                 .frame(maxWidth: UIScreen.main.bounds.width * 0.8)
                 .padding()
 
-            Text("WebOrg_v0.1.2")
+            Text("WebOrg_v0.1.3")
                 .font(.caption)
                 .foregroundColor(.secondary)
 
